14331d6b6075eee4d9d504efab7fc8ba07910520
Beatriz <beatriz.mediatto2@gmail.com>
2023-07-07 15:13:47 -0300

    feat: implementa entities Student

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import entities.Employee;
+import entities.Student;
 //import entities.Rectangle;
 
 import java.util.Locale;
@@ -8,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
+// RECTANGLE
 //        Rectangle rectangle = new Rectangle();
 //
 //        System.out.println("Enter rectangle width and height: ");
@@ -19,23 +20,41 @@ public class Main {
 //        System.out.printf("PERIMETER = %.2f%n", rectangle.Perimeter());
 //        System.out.printf("DIAGONAL = %.2f%n", rectangle.Diagnoal());
 
-        Employee employee = new Employee();
-        System.out.print("Name: ");
-        employee.name = sc.nextLine();
-        System.out.print("Gross salary: ");
-        employee.grossSalary = sc.nextDouble();
-        System.out.print("Tax: ");
-        employee.tax = sc.nextDouble();
-        System.out.println();
+        // EMPLOYEE
+//        Employee employee = new Employee();
+//        System.out.print("Name: ");
+//        employee.name = sc.nextLine();
+//        System.out.print("Gross salary: ");
+//        employee.grossSalary = sc.nextDouble();
+//        System.out.print("Tax: ");
+//        employee.tax = sc.nextDouble();
+//        System.out.println();
+//
+//        System.out.println("Employee: " + employee.toString());
+//        System.out.println();
+//        System.out.print("Which percentage to increase salary? ");
+//        double percentage = sc.nextDouble();
+//        employee.increaseSalary(percentage);
+//
+//        System.out.println();
+//        System.out.println("Update data: " + employee);
 
-        System.out.println("Employee: " + employee.toString());
-        System.out.println();
-        System.out.print("Which percentage to increase salary? ");
-        double percentage = sc.nextDouble();
-        employee.increaseSalary(percentage);
+        // STUDENT
+        Student student = new Student();
 
-        System.out.println();
-        System.out.println("Update data: " + employee);
+        student.name = sc.nextLine();
+        student.grade1 = sc.nextDouble();
+        student.grade2 = sc.nextDouble();
+        student.grade3 = sc.nextDouble();
+
+        System.out.printf("FINAL GRADE: %.2f%n", student.finalGrade());
+
+        if(student.finalGrade() < 60.0) {
+            System.out.println("FAILD");
+            System.out.printf("MISSING %.2f POINTS %n", student.missingPoints());
+        } else {
+            System.out.println("PASS");
+        }
 
         sc.close();
     }
